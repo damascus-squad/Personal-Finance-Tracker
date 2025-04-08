1d7bc70ca43bd4dfe86b531fbfaffaf9e6f21745
Yusuf Nasser <yusufnassereng@gmail.com>
2025-04-08 21:15:17 +0200

    Add summary class
    Class includes two required methods:
    - getByMonth: generates monthly report
    - getByYear: generates yearly report
    
    Co-authored-by: HarithBashar <harithbashar@gmail.com>

---
+++ src/main/kotlin/summary/Summary.kt
@@ -0,0 +1,27 @@
+package org.example.summary
+
+import org.example.model.FakeCategory
+import org.example.model.FakeTransaction
+import org.example.model.Report
+
+class Summary {
+    fun getByMonth(fakeTransactions: List<FakeTransaction>, month: Int, year: Int): Report {
+        return Report (
+            0,
+            0,
+            mapOf(
+                Pair(FakeCategory.SALARY, 0)
+            )
+        )
+    }
+
+    fun getByYear(fakeTransactions: List<FakeTransaction>, year: Int): Report {
+        return Report (
+            0,
+            0,
+            mapOf(
+                Pair(FakeCategory.SALARY, 0)
+            )
+        )
+    }
+}
