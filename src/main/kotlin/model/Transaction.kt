943180b2d0755a7496bb0923d689d05bffb2d86e
Asmaa Karam <asmaakaram793@gmai.com>
2025-04-10 15:34:32 +0200

    update id to be UUID type

--- src/main/kotlin/model/Transaction.kt
+++ src/main/kotlin/model/Transaction.kt
@@ -1,9 +1,10 @@
 package org.example
 
 import java.time.LocalDateTime
+import java.util.UUID
 
 data class Transaction(
-    val id: Int,
+    val id: UUID,
     var amount: Double,
     val transactionType: TransactionType,
     var date: LocalDateTime,
@@ -15,3 +16,4 @@ enum class TransactionType {
     INCOME,
     EXPENSE
 }
+
