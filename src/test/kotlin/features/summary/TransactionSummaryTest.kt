2548f828b9981c4e389cdec019b1e80ef755dc85
Harith Bashar <harithbashar@gmail.com>
2025-04-11 19:59:02 +0300

    Edit test cases with new refactoring

--- src/test/kotlin/features/summary/TransactionSummaryTest.kt
+++ src/test/kotlin/features/summary/TransactionSummaryTest.kt
@@ -5,7 +5,9 @@ import org.example.model.FakeTransaction
 import org.example.model.FakeTransactionType
 import org.example.model.TransactionReport
 import org.example.features.summary.TransactionSummary
+import org.example.model.CategorySummary
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 
 fun main() {
@@ -14,7 +16,7 @@ fun main() {
     // Yearly report cases
     checkReport(
         name = "Should return empty report, When empty list is entered",
-        result = summary.getByYear(
+        result = TransactionSummary(emptyList()).getByYear(
             year = 2025
         ),
         correctResult = TransactionReport(
@@ -25,214 +27,135 @@ fun main() {
     )
 
     checkReport(
-        name = "Should succeed, true process - Yearly",
-        result = summary.getByYear(
-            fakeTransactions = listOf(
+        name = "Should succeed, When using income amount - Yearly",
+        result = TransactionSummary(
+            listOf(
                 FakeTransaction(
-                    LocalDate.of(2025, 1, 8), 0.0,
+                    date = LocalDate.of(2025, 5, 21),
+                    amount = 50_000.0,
+                    transactionType = FakeTransactionType.INCOME,
                     category = FakeCategory.SALARY,
-                    transactionType = FakeTransactionType.INCOME
-                ),
-            ),
+                )
+            )
+        ).getByYear(
             year = 2025
         ),
         correctResult = TransactionReport(
-            0.0,
+            50_000.0,
             0.0,
             mutableMapOf(
-                Pair(FakeCategory.SALARY, 0.0)
+                FakeCategory.SALARY to CategorySummary(
+                    category = FakeCategory.SALARY,
+                    amount = 50_000.0,
+                    transactionsCount = 1,
+                )
             )
         )
     )
 
     checkReport(
-        name = "Should succeed, When the income amount is positive - Yearly",
-        result = summary.getByYear(
-            fakeTransactions = listOf(
+        name = "Should succeed, When using expense amount - Yearly",
+        result = TransactionSummary(
+            listOf(
                 FakeTransaction(
-                    LocalDate.of(2025, 4, 8), 12_000.0,
-                    category = FakeCategory.SALARY,
-                    transactionType = FakeTransactionType.INCOME
-                ),
-            ),
-            year = 2025
-        ),
-        correctResult = TransactionReport(
-            expenses = 0.0,
-            income = 12_000.0,
-            fakeCategorySummaries = mutableMapOf(Pair(FakeCategory.SALARY, 12_000.0))
-        )
-    )
-
-    checkReport(
-        name = "Should succeed, When the expenses amount is negative - Yearly",
-        result = summary.getByYear(
-            fakeTransactions = listOf(
-                FakeTransaction(
-                    LocalDate.of(2025, 4, 8), 200.0,
+                    date = LocalDate.of(2025, 5, 21),
+                    amount = 25_000.0,
+                    transactionType = FakeTransactionType.EXPENSE,
                     category = FakeCategory.RENT,
-                    transactionType = FakeTransactionType.EXPENSE
                 ),
-            ),
+                FakeTransaction(
+                    date = LocalDate.of(2025, 5, 21),
+                    amount = 25_000.0,
+                    transactionType = FakeTransactionType.EXPENSE,
+                    category = FakeCategory.RENT,
+                ),
+            )
+        ).getByYear(
             year = 2025
         ),
         correctResult = TransactionReport(
-            expenses = 200.0,
-            income = 0.0,
-            fakeCategorySummaries = mutableMapOf(Pair(FakeCategory.RENT, 200.0))
+            0.0,
+            50_000.0,
+            mutableMapOf(
+                FakeCategory.RENT to CategorySummary(
+                    category = FakeCategory.RENT,
+                    amount = 50_000.0,
+                    transactionsCount = 2,
+                ),
+            )
         )
     )
 
     checkReport(
         name = "Should succeed, When the report type is yearly - Yearly",
-        result = summary.getByYear(
-            fakeTransactions = listOf(
+        result = TransactionSummary(
+            listOf(
                 FakeTransaction(
-                    LocalDate.of(2025, 4, 8), 12_000.0,
+                    date = LocalDate.of(2025, 5, 21),
+                    amount = 20_000.0,
+                    transactionType = FakeTransactionType.INCOME,
                     category = FakeCategory.SALARY,
-                    transactionType = FakeTransactionType.INCOME
                 ),
-            ),
+                FakeTransaction(
+                    date = LocalDate.of(2025, 5, 21),
+                    amount = 40_000.0,
+                    transactionType = FakeTransactionType.EXPENSE,
+                    category = FakeCategory.FOOD,
+                )
+            )
+        ).getByYear(
             year = 2025
         ),
         correctResult = TransactionReport(
-            expenses = 0.0,
-            income = 12_000.0,
-            fakeCategorySummaries = mutableMapOf(Pair(FakeCategory.SALARY, 12_000.0))
+            20_000.0,
+            40_000.0,
+            mutableMapOf(
+                FakeCategory.SALARY to CategorySummary(
+                    category = FakeCategory.SALARY,
+                    amount = 20_000.0,
+                    transactionsCount = 1,
+                ),
+                FakeCategory.FOOD to CategorySummary(
+                    category = FakeCategory.FOOD,
+                    amount = 40_000.0,
+                    transactionsCount = 1,
+                )
+            )
         )
     )
 
     checkReport(
         name = "Should fail, When the wrong category is reported - Yearly",
-        result = summary.getByYear(
-            fakeTransactions = listOf(
+        result = TransactionSummary(
+            listOf(
                 FakeTransaction(
-                    LocalDate.of(2025, 4, 8), 2_000.0,
-                    category = FakeCategory.FOOD,
-                    transactionType = FakeTransactionType.EXPENSE
-                ),
-            ),
-            year = 2025
-        ),
-        correctResult = TransactionReport(
-            expenses = 2_000.0,
-            income = 0.0,
-            fakeCategorySummaries = mutableMapOf(Pair(FakeCategory.FOOD, 2_000.0))
-        )
-    )
-
-    // Monthly report cases
-
-    checkReport(
-        name = "Should succeed, true process - Monthly",
-        result = summary.getByMonth(
-            fakeTransactions = listOf(
-                FakeTransaction(
-                    LocalDate.of(2025, 1, 8), 0.0,
+                    date = LocalDate.of(2025, 5, 21),
+                    amount = 50_000.0,
+                    transactionType = FakeTransactionType.INCOME,
                     category = FakeCategory.SALARY,
-                    transactionType = FakeTransactionType.INCOME
-                ),
-            ),
-            month = 1,
+                )
+            )
+        ).getByYear(
             year = 2025
         ),
         correctResult = TransactionReport(
-            0.0,
+            50_000.0,
             0.0,
             mutableMapOf(
-                Pair(FakeCategory.SALARY, 0.0)
+                FakeCategory.SALARY to CategorySummary(
+                    category = FakeCategory.SALARY,
+                    amount = 50_000.0,
+                    transactionsCount = 1,
+                )
             )
         )
-
     )
-
-    checkReport(
-        name = "Should succeed, When the income amount is positive - Monthly",
-        result = summary.getByMonth(
-            fakeTransactions = listOf(
-                FakeTransaction(
-                    LocalDate.of(2025, 4, 8), 12_000.0,
-                    category = FakeCategory.SALARY,
-                    transactionType = FakeTransactionType.INCOME
-                ),
-            ),
-            year = 2025,
-            month = 4
-        ),
-        correctResult = TransactionReport(
-            expenses = 0.0,
-            income = 12_000.0,
-            fakeCategorySummaries = mutableMapOf(Pair(FakeCategory.SALARY, 12_000.0))
-        )
-    )
-
-    checkReport(
-        name = "Should succeed, When the expenses amount is negative - Monthly",
-        result = summary.getByMonth(
-            fakeTransactions = listOf(
-                FakeTransaction(
-                    LocalDate.of(2025, 4, 8), 200.0,
-                    category = FakeCategory.RENT,
-                    transactionType = FakeTransactionType.EXPENSE
-                ),
-            ),
-            year = 2025,
-            month = 4
-        ),
-        correctResult = TransactionReport(
-            expenses = 200.0,
-            income = 0.0,
-            fakeCategorySummaries = mutableMapOf(Pair(FakeCategory.RENT, 200.0))
-        )
-    )
-
-    checkReport(
-        name = "Should succeed, When the report type is monthly - Monthly",
-        result = summary.getByMonth(
-            fakeTransactions = listOf(
-                FakeTransaction(
-                    LocalDate.of(2025, 4, 8), 12_000.0,
-                    category = FakeCategory.SALARY,
-                    transactionType = FakeTransactionType.INCOME
-                ),
-            ),
-            year = 2025,
-            month = 4
-        ),
-        correctResult = TransactionReport(
-            expenses = 0.0,
-            income = 12_000.0,
-            fakeCategorySummaries = mutableMapOf(Pair(FakeCategory.SALARY, 12_000.0))
-        )
-    )
-
-    checkReport(
-        name = "Should succeed, When the correct category is reported - Monthly",
-        result = summary.getByMonth(
-            fakeTransactions = listOf(
-                FakeTransaction(
-                    LocalDate.of(2025, 4, 8), 2_000.0,
-                    category = FakeCategory.RENT,
-                    transactionType = FakeTransactionType.EXPENSE
-                ),
-            ),
-            year = 2025,
-            month = 4
-        ),
-        correctResult = TransactionReport(
-            expenses = 2_000.0,
-            income = 0.0,
-            fakeCategorySummaries = mutableMapOf(Pair(FakeCategory.RENT, 2_000.0))
-        )
-    )
-
+//
+//    // Monthly report cases
+//
     checkReport(
         name = "Should return empty report, When empty list is entered - Monthly",
-        result = summary.getByMonth(
-            fakeTransactions = emptyList(),
-            year = 2025,
-            month = 5
-        ),
+        result = TransactionSummary(emptyList()).getByMonth(2025, 5),
         correctResult = TransactionReport(
             0.0,
             0.0,
@@ -240,16 +163,135 @@ fun main() {
         )
     )
 
+    checkReport(
+        name = "Should succeed, When using income amount - Monthly",
+        result = TransactionSummary(
+            listOf(
+                FakeTransaction(
+                    date = LocalDate.of(2025, 5, 21),
+                    amount = 50_000.0,
+                    transactionType = FakeTransactionType.INCOME,
+                    category = FakeCategory.SALARY,
+                )
+            )
+        ).getByMonth(2025, 5),
+        correctResult = TransactionReport(
+            50_000.0,
+            0.0,
+            mutableMapOf(
+                FakeCategory.SALARY to CategorySummary(
+                    category = FakeCategory.SALARY,
+                    amount = 50_000.0,
+                    transactionsCount = 1,
+                )
+            )
+        )
+    )
+
+    checkReport(
+        name = "Should succeed, When using expense amount - Monthly",
+        result = TransactionSummary(
+            listOf(
+                FakeTransaction(
+                    date = LocalDate.of(2025, 5, 21),
+                    amount = 25_000.0,
+                    transactionType = FakeTransactionType.EXPENSE,
+                    category = FakeCategory.RENT,
+                ),
+                FakeTransaction(
+                    date = LocalDate.of(2025, 5, 21),
+                    amount = 25_000.0,
+                    transactionType = FakeTransactionType.EXPENSE,
+                    category = FakeCategory.RENT,
+                ),
+            )
+        ).getByMonth(2025, 5),
+        correctResult = TransactionReport(
+            0.0,
+            50_000.0,
+            mutableMapOf(
+                FakeCategory.RENT to CategorySummary(
+                    category = FakeCategory.RENT,
+                    amount = 50_000.0,
+                    transactionsCount = 2,
+                ),
+            )
+        )
+    )
+
+    checkReport(
+        name = "Should succeed, When the report type is monthly",
+        result = TransactionSummary(
+            listOf(
+                FakeTransaction(
+                    date = LocalDate.of(2025, 5, 21),
+                    amount = 20_000.0,
+                    transactionType = FakeTransactionType.INCOME,
+                    category = FakeCategory.SALARY,
+                ),
+                FakeTransaction(
+                    date = LocalDate.of(2025, 5, 21),
+                    amount = 40_000.0,
+                    transactionType = FakeTransactionType.EXPENSE,
+                    category = FakeCategory.FOOD,
+                )
+            )
+        ).getByMonth(2025, 5),
+        correctResult = TransactionReport(
+            20_000.0,
+            40_000.0,
+            mutableMapOf(
+                FakeCategory.SALARY to CategorySummary(
+                    category = FakeCategory.SALARY,
+                    amount = 20_000.0,
+                    transactionsCount = 1,
+                ),
+                FakeCategory.FOOD to CategorySummary(
+                    category = FakeCategory.FOOD,
+                    amount = 40_000.0,
+                    transactionsCount = 1,
+                )
+            )
+        )
+    )
+
+    checkReport(
+        name = "Should fail, When the wrong category is reported - Monthly",
+        result = TransactionSummary(
+            listOf(
+                FakeTransaction(
+                    date = LocalDate.of(2025, 5, 21),
+                    amount = 50_000.0,
+                    transactionType = FakeTransactionType.INCOME,
+                    category = FakeCategory.SALARY,
+                )
+            )
+        ).getByMonth(2025, 5),
+        correctResult = TransactionReport(
+            50_000.0,
+            0.0,
+            mutableMapOf(
+                FakeCategory.SALARY to CategorySummary(
+                    category = FakeCategory.SALARY,
+                    amount = 50_000.0,
+                    transactionsCount = 1,
+                )
+            )
+        )
+    )
 }
 
 fun checkReport(
     name: String,
     result: TransactionReport,
-    correctResult: TransactionReport
+    correctResult: TransactionReport,
 ) {
     if (result == correctResult) {
         println("\uD83D\uDFE9 Success - $name")
     } else {
+        println("===============")
+        println(result)
+        println(correctResult)
         println("\uD83D\uDFE5 Failed - $name")
     }
 }
