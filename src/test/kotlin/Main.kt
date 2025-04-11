0c6ea22b2bfc5efd76189a124159c04cecb62f79
ameeramjed <pr.ameer98@gmail.com>
2025-04-11 21:37:36 +0300

    add check test cases

---
+++ src/test/kotlin/Main.kt
@@ -0,0 +1,14 @@
+import util.runAllCheckTests
+
+fun main() {
+    val isAllTestCasedPassed = runAllCheckTests(
+        listOf<Any>()
+    )
+
+    if (!isAllTestCasedPassed) {
+        println("❌ Some tests failed.")
+        kotlin.system.exitProcess(1)
+    } else {
+        println("✅ All tests passed.")
+    }
+}
